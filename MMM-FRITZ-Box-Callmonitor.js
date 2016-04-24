57555350f0ee24cacd131ce3dbed63f84db7bb6c
Paul-Vincent Roll <paviro@me.com>
2016-04-24 19:06:28 +0200

    Merge 3449621 back in.

--- MMM-FRITZ-Box-Callmonitor.js
+++ MMM-FRITZ-Box-Callmonitor.js
@@ -7,13 +7,13 @@
  * MIT Licensed.
  */
 
-Module.register('MMM-FRITZ-Box-Callmonitor',{
+Module.register("MMM-FRITZ-Box-Callmonitor",{
 	
 	// Default module config.
 	defaults: {
 		NumberFontSize: "30px",
 		vCard: false,
-		fritzIP: '192.168.178.1',
+		fritzIP: "192.168.178.1",
 		fritzPort: 1012
 	},
 	
@@ -27,15 +27,15 @@ Module.register('MMM-FRITZ-Box-Callmonitor',{
 	
 	// Override socket notification handler.
 	socketNotificationReceived: function(notification, payload) {
-		if (notification === 'call') {
-			if (payload != 'clear'){
+		if (notification === "call") {
+			if (payload != "clear"){
 				this.sendNotification("SHOW_ALERT", {
 					title: this.translate("title"),
 					message: "<span style='font-size:" + this.config.NumberFontSize + "'>" + payload + "<span>",
 					imageFA: "phone"
 				}); 
 			}
-			if (payload == 'clear'){
+			if (payload == "clear"){
 				this.sendNotification("HIDE_ALERT");
 			}
 			
@@ -44,8 +44,8 @@ Module.register('MMM-FRITZ-Box-Callmonitor',{
 	
 	start: function() {
 		//Open Socket connection
-		this.sendSocketNotification('CONFIG', this.config);
-		Log.info('Starting module: ' + this.name);
+		this.sendSocketNotification("CONFIG", this.config);
+		Log.info("Starting module: " + this.name);
 		}
 
 });
--- node_helper.js
+++ node_helper.js
@@ -1,19 +1,19 @@
-'use strict';
-const NodeHelper = require('node_helper');
-const CallMonitor = require('node-fritzbox-callmonitor');
-var vcard = require('vcard-json');
-var phone = require('phone-formatter');
+"use strict";
+const NodeHelper = require("node_helper");
+const CallMonitor = require("node-fritzbox-callmonitor");
+var vcard = require("vcard-json");
+var phone = require("phone-formatter");
 
 module.exports = NodeHelper.create({
   // Subclass start method.
   start: function() {
     this.started = false
-    console.log('Starting module: ' + this.name);
+    console.log("Starting module: " + this.name);
   },
   
   getName: function (number) {
     //Normalize number
-    var number_formatted = phone.normalize(number.replace(/\s/g, ''))
+    var number_formatted = phone.normalize(number.replace(/\s/g, ""))
     //Check if number is in AdressBook if yes return the name
     if (number_formatted in this.AddressBook){
       return this.AddressBook[number_formatted]
@@ -26,14 +26,14 @@ module.exports = NodeHelper.create({
   
   socketNotificationReceived: function(notification, payload) {
     //Received config from client
-    if(notification === 'CONFIG') {
+    if(notification === "CONFIG") {
       //set config to config send by client
       this.config = payload
       //if monitor has not been started before (makes sure it does not get started again if the web interface is reloaded)
       if(!this.started) {
-        //set started to true, so it won't start again
+        //set started to true, so it won"t start again
         this.started = true;
-        console.log('Received config for ' + this.name);
+        console.log("Received config for " + this.name);
         //helper variable so that the module-this is available inside our monitor
         var self = this;
         //Set up CallMonitor with config received from client
@@ -46,7 +46,7 @@ module.exports = NodeHelper.create({
           //Parse contact file
           vcard.parseVcardFile(this.config.vCard, function(err, data){
             //In case there is an error reading the vcard file
-            if(err) console.log('[' + this.name + '] ' + err);
+            if(err) console.log("[" + this.name + "] " + err);
             //success
             else {
               //For each contact in vcf file
@@ -54,7 +54,7 @@ module.exports = NodeHelper.create({
                 //For each phone number in contact
                 for (var a = 0; a < data[i].phone.length; a++) {
                     //normalize and add to AddressBook
-                    self.AddressBook[phone.normalize(data[i].phone[a].value.replace(/\s/g, ''))] = data[i].fullname
+                    self.AddressBook[phone.normalize(data[i].phone[a].value.replace(/\s/g, ""))] = data[i].fullname
                   }
                 }
             }
@@ -62,26 +62,26 @@ module.exports = NodeHelper.create({
         }
         
         //Incoming call
-        monitor.on('inbound', function (call) {
+        monitor.on("inbound", function (call) {
             //If caller is not empty
             if (call.caller != "") {
                 //if path to contact file is given
                 if (self.config.vCard){
                   //Get name via getName() and send to interface
-                  self.sendSocketNotification('call', self.getName(call.caller));
+                  self.sendSocketNotification("call", self.getName(call.caller));
                 }
                 //path to contact file is not given
                 else {
                   //send raw number to interface
-                  self.sendSocketNotification('call', call.caller);
+                  self.sendSocketNotification("call", call.caller);
                 }
             };
         });
 
         //Caller disconnected / Call accepted
-        monitor.on('disconnected', function (call) {
+        monitor.on("disconnected", function (call) {
              //send clear command to interface
-             self.sendSocketNotification('call', 'clear');
+             self.sendSocketNotification("call", "clear");
         });
         console.log(this.name + " is waiting for incoming calls.");
       };
