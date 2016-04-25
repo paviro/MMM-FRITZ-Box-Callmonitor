1340e9561ce13f9ecb0ef33fce83461028d7988d
Paul-Vincent Roll <paviro@me.com>
2016-04-25 17:17:24 +0200

    js style.

--- MMM-FRITZ-Box-Callmonitor.js
+++ MMM-FRITZ-Box-Callmonitor.js
@@ -7,8 +7,8 @@
  * MIT Licensed.
  */
 
-Module.register("MMM-FRITZ-Box-Callmonitor",{
-	
+Module.register("MMM-FRITZ-Box-Callmonitor", {
+
 	// Default module config.
 	defaults: {
 		NumberFontSize: 30,
@@ -20,9 +20,9 @@ Module.register("MMM-FRITZ-Box-Callmonitor",{
 		maximumCalls: 5,
 		fade: true,
 		fadePoint: 0.25
-		
+
 	},
-	
+
 	// Define required translations.
 	getTranslations: function() {
 		return {
@@ -30,110 +30,109 @@ Module.register("MMM-FRITZ-Box-Callmonitor",{
 			de: "translations/de.json"
 		};
 	},
-	
+
 	getScripts: function() {
 		return ["moment.js"];
 	},
-	
+
 	// Override socket notification handler.
 	socketNotificationReceived: function(notification, payload) {
 		if (notification === "call") {
-				//Show alert on UI
-				this.sendNotification("SHOW_ALERT", {
-					title: this.translate("title"),
-					message: "<span style='font-size:" + this.config.NumberFontSize.toString() + "px'>" + payload + "<span>",
-					imageFA: "phone"
-				});
-				
-				//Set active Alert to current call
-				this.activeAlert = payload
+			//Show alert on UI
+			this.sendNotification("SHOW_ALERT", {
+				title: this.translate("title"),
+				message: "<span style='font-size:" + this.config.NumberFontSize.toString() + "px'>" + payload + "<span>",
+				imageFA: "phone"
+			});
+
+			//Set active Alert to current call
+			this.activeAlert = payload;
 		}
-			if (notification === "connected"){
-				//Send notification for currentCall module
-				this.sendNotification("CALL_CONNECTED", payload);
-				
-				//Remove alert only on connect if it is the current alert shown
-				if (this.activeAlert === payload){
-					//Remove alert from UI when call is connected
-					this.sendNotification("HIDE_ALERT");
-					this.activeAlert = null
-				}
+		if (notification === "connected") {
+			//Send notification for currentCall module
+			this.sendNotification("CALL_CONNECTED", payload);
+
+			//Remove alert only on connect if it is the current alert shown
+			if (this.activeAlert === payload) {
+				//Remove alert from UI when call is connected
+				this.sendNotification("HIDE_ALERT");
+				this.activeAlert = null;
 			}
-			if (notification === "disconnected"){
-				//Send notification for currentCall module
-				this.sendNotification("CALL_DISCONNECTED", payload.caller);
-				
-				//Add call to call_history (timestamp and caller) or if minimumCallLength is set only missed calls
-				if (payload.duration <= this.config.minimumCallLength){
-					this.call_history.push({"time": moment(), "caller": payload.caller})	
-				}
-				//Update call list on UI
-				this.updateDom(3000);
-				
-				//Remove alert only on disconnect if it is the current alert shown
-				if (this.activeAlert === payload.caller){
-					//Remove alert from UI when call is connected
-					this.sendNotification("HIDE_ALERT");
-					this.activeAlert = null
-				}
+		}
+		if (notification === "disconnected") {
+			//Send notification for currentCall module
+			this.sendNotification("CALL_DISCONNECTED", payload.caller);
+
+			//Add call to call_history (timestamp and caller) or if minimumCallLength is set only missed calls
+			if (payload.duration <= this.config.minimumCallLength) {
+				this.call_history.push({"time": moment(), "caller": payload.caller});
 			}
-			
-		
+			//Update call list on UI
+			this.updateDom(3000);
+
+			//Remove alert only on disconnect if it is the current alert shown
+			if (this.activeAlert === payload.caller) {
+				//Remove alert from UI when call is connected
+				this.sendNotification("HIDE_ALERT");
+				this.activeAlert = null;
+			}
+		}
+
 	},
-	
+
 	start: function() {
 		//Create call_history array
-		this.call_history = []
-		this.activeAlert = null
+		this.call_history = [];
+		this.activeAlert = null;
 		//Set helper variable this so it is available in the timer
 		var self = this;
 		//Update doom every minute so that the time of the call updates and calls get removed after a certain time
 		setInterval(function() {
 			self.updateDom();
 		}, 60000);
-		
+
 		//Send config to the node helper
 		this.sendSocketNotification("CONFIG", this.config);
 		Log.info("Starting module: " + this.name);
 	},
-	
+
 	getDom: function() {
 		//For each call in call_history
 		for (var i = 0; i < this.call_history.length; i++) {
 			//Check if call is older than maximumCallDistance
-			if ( moment(moment()).diff(moment(this.call_history[i].time)) > this.config.maximumCallDistance * 60000 ){
+			if (moment(moment()).diff(moment(this.call_history[i].time)) > this.config.maximumCallDistance * 60000) {
 				//is older -> remove from list
 				this.call_history.splice(i, 1);
+			}
 		}
-	}
 		//get latest x calls configured by maximumCalls
 		var calls = this.call_history.slice(this.call_history.length - this.config.maximumCalls, this.call_history.length);
-		
+
 		//Create table
 		var wrapper = document.createElement("table");
 		//set table style
 		wrapper.className = "small";
-		
+
 		//If there are no calls, set "noCall" text.
 		if (calls.length === 0) {
 			wrapper.innerHTML = this.translate("noCall");
 			wrapper.className = "xsmall dimmed";
 			return wrapper;
 		}
-		
+
 		//For each call in calls
 		for (var i = 0; i < calls.length; i++) {
-			
+
 			//Create callWrapper
 			var callWrapper = document.createElement("tr");
 			callWrapper.className = "normal";
-			
+
 			//Set caller of row
 			var caller =  document.createElement("td");
 			caller.innerHTML = calls[i].caller;
 			caller.className = "title bright";
 			callWrapper.appendChild(caller);
-			
+
 			//Set time of row
 			var time =  document.createElement("td");
 			time.innerHTML = moment(calls[i].time).fromNow();
@@ -142,8 +141,8 @@ Module.register("MMM-FRITZ-Box-Callmonitor",{
 
 			//Add to wrapper
 			wrapper.appendChild(callWrapper);
-			
-			
+
+
 			// Create fade effect by MichMich (MIT)
 			if (this.config.fade && this.config.fadePoint < 1) {
 				if (this.config.fadePoint < 0) {
--- node_helper.js
+++ node_helper.js
@@ -7,80 +7,80 @@ const phone = require("phone-formatter");
 module.exports = NodeHelper.create({
   // Subclass start method.
   start: function() {
-    this.started = false
-    //create adressbook dictionary 
-    this.AddressBook = {}
+    this.started = false;
+    //create adressbook dictionary
+    this.AddressBook = {};
     console.log("Starting module: " + this.name);
-  },
-  
-  getName: function (number) {
+			},
+
+  getName: function(number) {
     //Normalize number
-    var number_formatted = phone.normalize(number.replace(/\s/g, ""))
+    var number_formatted = phone.normalize(number.replace(/\s/g, ""));
     //Check if number is in AdressBook if yes return the name
-    if (number_formatted in this.AddressBook){
-      return this.AddressBook[number_formatted]
+    if (number_formatted in this.AddressBook) {
+					return this.AddressBook[number_formatted];
     }
     //Not in AdressBook return original number
     else {
-      return number
+					return number;
     }
-  },
-  
+			},
+
   socketNotificationReceived: function(notification, payload) {
     //Received config from client
-    if(notification === "CONFIG") {
-      //set config to config send by client
-      this.config = payload
-      //if monitor has not been started before (makes sure it does not get started again if the web interface is reloaded)
-      if(!this.started) {
-        //set started to true, so it won"t start again
-        this.started = true;
-        console.log("Received config for " + this.name);
-        //helper variable so that the module-this is available inside our monitor
-        var self = this;
-        //Set up CallMonitor with config received from client
-        var monitor = new CallMonitor(this.config.fritzIP, this.config.fritzPort);
-        
-        //If path to contact file is given
-        if (this.config.vCard){
-          //Parse contact file
-          vcard.parseVcardFile(this.config.vCard, function(err, data){
-            //In case there is an error reading the vcard file
-            if(err) console.log("[" + this.name + "] " + err);
-            //success
-            else {
-              //For each contact in vcf file
-              for (var i = 0; i < data.length; i++) {
-                //For each phone number in contact
-                for (var a = 0; a < data[i].phone.length; a++) {
-                    //normalize and add to AddressBook
-                    self.AddressBook[phone.normalize(data[i].phone[a].value.replace(/\s/g, ""))] = data[i].fullname
-                  }
-                }
-            }
-          })
-        }
-        
-        //Incoming call
-        monitor.on("inbound", function (call) {
-            //If caller is not empty
-            if (call.caller != "") {
-                self.sendSocketNotification("call", self.getName(call.caller));
-            };
-        });
-        
-        //Call accepted
-        monitor.on('connected', function (call) {
-          self.sendSocketNotification("connected", self.getName(call.caller));
-        });
+    if (notification === "CONFIG") {
+					//set config to config send by client
+					this.config = payload;
+					//if monitor has not been started before (makes sure it does not get started again if the web interface is reloaded)
+					if (!this.started) {
+						//set started to true, so it won"t start again
+						this.started = true;
+						console.log("Received config for " + this.name);
+						//helper variable so that the module-this is available inside our monitor
+						var self = this;
+						//Set up CallMonitor with config received from client
+						var monitor = new CallMonitor(this.config.fritzIP, this.config.fritzPort);
 
-        //Caller disconnected
-        monitor.on("disconnected", function (call) {
-             //send clear command to interface
-             self.sendSocketNotification("disconnected", {"caller":self.getName(call.caller), "duration": call.duration});
-        });
-        console.log(this.name + " is waiting for incoming calls.");
-      };
+						//If path to contact file is given
+						if (this.config.vCard) {
+							//Parse contact file
+							vcard.parseVcardFile(this.config.vCard, function(err, data) {
+									//In case there is an error reading the vcard file
+									if (err) console.log("[" + this.name + "] " + err);
+									//success
+									else {
+										//For each contact in vcf file
+										for (var i = 0; i < data.length; i++) {
+											//For each phone number in contact
+											for (var a = 0; a < data[i].phone.length; a++) {
+												//normalize and add to AddressBook
+												self.AddressBook[phone.normalize(data[i].phone[a].value.replace(/\s/g, ""))] = data[i].fullname;
+											}
+										}
+									}
+								});
+						}
+
+						//Incoming call
+						monitor.on("inbound", function(call) {
+										//If caller is not empty
+										if (call.caller != "") {
+											self.sendSocketNotification("call", self.getName(call.caller));
+										};
+									});
+
+						//Call accepted
+						monitor.on("connected", function(call) {
+								self.sendSocketNotification("connected", self.getName(call.caller));
+							});
+
+						//Caller disconnected
+						monitor.on("disconnected", function(call) {
+											//send clear command to interface
+											self.sendSocketNotification("disconnected", {"caller": self.getName(call.caller), "duration": call.duration});
+										});
+						console.log(this.name + " is waiting for incoming calls.");
+					};
     };
-  }
-});
+			}
+	});
