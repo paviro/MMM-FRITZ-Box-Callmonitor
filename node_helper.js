cb68e01b54fad61d4ea67defb927dbbfce5c8586
Joseph Bethge <joseph.bethge@student.hpi.uni-potsdam.de>
2016-08-08 15:59:58 +0200

    progress in reading and parsing xml

--- node_helper.js
+++ node_helper.js
@@ -1,10 +1,20 @@
 "use strict";
+
 const NodeHelper = require("node_helper");
 const CallMonitor = require("node-fritzbox-callmonitor");
 const vcard = require("vcard-json");
 const phone = require("phone-formatter");
 const tr = require("tr-064");
 const xml2js = require("xml2js");
+const https = require("https");
+const url = require('url');
+
+const CALL_TYPE = Object.freeze({
+	INCOMING : 1,
+	MISSED : 2,
+	OUTGOING : 3
+})
+// outgoing missed calls are not in the list
 
 module.exports = NodeHelper.create({
 	// Subclass start method.
@@ -15,9 +25,13 @@ module.exports = NodeHelper.create({
 		console.log("Starting module: " + this.name);
 	},
 
+	normalizePhoneNumber(number) {
+
+	},
+
 	getName: function(number) {
 		//Normalize number
-		var number_formatted = phone.normalize(number.replace(/\s/g, ""));
+		var number_formatted = self.normalizePhoneNumber(number);
 		//Check if number is in AdressBook if yes return the name
 		if (number_formatted in this.AddressBook) {
 			return this.AddressBook[number_formatted];
@@ -87,41 +101,138 @@ module.exports = NodeHelper.create({
 					//For each phone number in contact
 					for (var a = 0; a < data[i].phone.length; a++) {
 						//normalize and add to AddressBook
-						self.AddressBook[phone.normalize(data[i].phone[a].value.replace(/\s/g, ""))] = data[i].fullname;
+						self.AddressBook[self.normalizePhoneNumber(data[i].phone[a].value)] = data[i].fullname;
 					}
 				}
 			}
 		});
 	},
 
+// https://192.168.178.1:49443/phonebook.lua?sid=daf3d377ab4c27ad&pbid=0
+// https://192.168.178.1:49443/calllist.lua?sid=daf3d377ab4c27ad
+
+	insecureBoxRequest(self, targetUrl, callback) {
+		// WARNING: use this method ONLY for requests to your fritz.box
+		// it ignores self-signed certificates
+		// the fritz.box uses this kind of generated certificate
+		var parsedUrl = url.parse(targetUrl);
+		var options = { 
+			protocol: parsedUrl.protocol,
+			host: parsedUrl.hostname,
+			port: parsedUrl.port,
+			path: parsedUrl.path,
+			rejectUnauthorized: false,
+			requestCert: true,
+			agent: false
+		};
+		return https.get(options, function(response) {
+			if (response.statusCode !== 200)
+			{
+				console.error("Error code " + response.statusCode + " on request: " + targetUrl);
+			}
+			// Continuously update stream with data
+			var body = '';
+			response.on('data', function(d) {
+				body += d;
+			});
+			response.on('end', function() {
+				callback(self, body);
+			});
+		});
+	},
+
+	loadCallList: function(self, body) {
+		xml2js.parseString(body, function (err, result) {
+			if (err) {
+				console.error(self.name + " error while parsing call list: " + err);
+				return;
+			}
+			var callArray = result.root.Call;
+			for (var index in callArray)
+			{
+				var call = callArray[index];
+				if (call.Type == CALL_TYPE.MISSED)
+				{
+					console.log(call.Id);
+				}
+			}
+		});
+	},
+
+	loadPhonebook: function(self, body) {
+		console.log(self.AddressBook);
+
+		xml2js.parseString(body, function (err, result) {
+			if (err) {
+				console.error(self.name + " error while parsing phonebook: " + err);
+				return;
+			}
+			var contactsArray = result.phonebooks.phonebook[0].contact;
+			for (var index in contactsArray)
+			{
+				var contact = contactsArray[index];
+
+
+				var contactNumbers = contact.telephony[0].number;
+				var contactName = contact.person[0].realName;
+
+				console.log(self.AddressBook);
+				for (var index in contactNumbers)
+				{
+					var currentNumber = contactNumbers[index]._;
+					console.log(currentNumber + " -> " + contactName);
+					self.AddressBook[self.normalizePhoneNumber(currentNumber)] = contactName;
+				}
+			}
+		});
+	},
+
 	setupApiAccess: function() {
-		//set up tr064 api access from config received
+		var self = this;
+
 		var tr064Api = new tr.TR064();
-		tr064Api.initTR064Device(this.config.fritzIP, this.config.tr064Port, function (err, device) {
-			if (!err) {
-				device.startEncryptedCommunication(function (err, sslDev) {
-					if (!err) {
-						sslDev.login(this.config.username, this.config.password);
-						var phoneservice = sslDev.services["urn:dslforum-org:service:X_AVM-DE_OnTel:1"];
-						phoneservice.actions.GetCallList(function (err, result) {
-							if (!err) {
-								callListUrl = result["NewCallListURL"];
-								console.log(callListUrl);
-							}
-						});
-						phoneservice.actions.GetPhonebookList(function (err, result) {
-							if (!err) {
-								phonebooks = result["NewPhonebookList"];
-								console.log(phonebooks);
-							}
-						});
-						phoneservice.actions.GetPhonebook({'NewPhonebookID' : 0}, function (err, result) {
-							console.log(result);
-							console.log(err);
-						});
+		tr064Api.initTR064Device(self.config.fritzIP, self.config.tr064Port, function (err, device) {
+			if (err) {
+				console.error(self.name + " error: " + err);
+				return;
+			}
+			device.startEncryptedCommunication(function (err, sslDev) {
+				if (err) {
+					console.error(self.name + " error: " + err);
+					return;
+				}
+				sslDev.login(self.config.username, self.config.password);
+				console.log(self.name + " login");
+				var phoneservice = sslDev.services["urn:dslforum-org:service:X_AVM-DE_OnTel:1"];
+				phoneservice.actions.GetCallList(function (err, result) {
+					if (err) {
+						console.error(self.name + " error: " + err);
+						return;
+					}
+					self.insecureBoxRequest(self, result["NewCallListURL"], self.loadCallList);
+				});
+				phoneservice.actions.GetPhonebookList(function (err, result) {
+					if (err) {
+						console.error(self.name + " error: " + err);
+						return;
+					}
+					var phonebooks = result["NewPhonebookList"];
+
+					for (var phonebookID in phonebooks.split(",")) {
+						var filter = self.config.loadSpecificPhonebook;
+						if (filter == "" || filter == phonebookID)
+						{
+							phoneservice.actions.GetPhonebook({'NewPhonebookID' : phonebookID}, function (err, result) {
+								if (err) {
+									console.error(self.name + " error: " + err);
+									return;
+								}
+								self.insecureBoxRequest(self, result["NewPhonebookURL"], self.loadPhonebook);
+							});
+						}
 					}
 				});
-			}
+			});
 		});
 	}
 });
